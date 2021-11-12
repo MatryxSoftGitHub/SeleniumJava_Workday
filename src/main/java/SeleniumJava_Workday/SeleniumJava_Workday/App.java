b01b17554379a76d948f1ef91d5303b157c4d5a3
Supritha Ravindran <supritha@ad.matryxsoft.com>
2021-11-12 15:10:57 +0530

    First commit

---
+++ src/main/java/SeleniumJava_Workday/SeleniumJava_Workday/App.java
@@ -0,0 +1,13 @@
+package SeleniumJava_Workday.SeleniumJava_Workday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
